eead216f0b046e3a6f0a10405c494f78483db0c7
Dmytro Chekhovskyi <64646103+TheDeiw@users.noreply.github.com>
2026-02-17 11:32:28 +0100

    feat: add finish scripts

---
+++ Assets/Scripts/ExecuteFinish.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Level_1_Scripts
+{
+    public class ExecuteFinish : MonoBehaviour
+    {
+        [SerializeField] GameObject winCanvas;
+        [SerializeField] GameObject loseCanvas;
+        [SerializeField] GameObject player;
+
+        void Start()
+        {
+            winCanvas.SetActive(false);
+            loseCanvas.SetActive(false);
+        }
+
+        public void FinishGame(int result)
+        {
+            if (result == 0)
+            {
+                loseCanvas.SetActive(true);
+            }
+            else if (result == 1)
+            {
+                winCanvas.SetActive(true);
+            }
+            var moveProvider = player.GetComponent<UnityEngine.XR.Interaction.Toolkit.Locomotion.Movement.ContinuousMoveProvider>();
+            if (moveProvider != null)
+                moveProvider.enabled = false;
+        }
+        
+        
+    }
+}

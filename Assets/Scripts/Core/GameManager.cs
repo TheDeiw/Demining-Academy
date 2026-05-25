f3efb0f9deebbef9e714f2890338570d407b541a
TheDeiw <64646103+TheDeiw@users.noreply.github.com>
2026-05-25 17:28:05 +0300

    Refactor: Add VR mode switcher, Add Zenject plugin and adjusted DI in GameManager

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace DeminingAcademy.Core
+{
+    public class GameManager : IInitializable
+    {
+        private readonly IModeSwitcherService _modeSwitcher;
+
+        public GameManager(IModeSwitcherService modeSwitcher)
+        {
+            _modeSwitcher = modeSwitcher;
+        }
+
+        public void Initialize()
+        {
+            _modeSwitcher.ApplySelectedMode();
+        
+            Debug.Log("GameManager successfully applied.");
+        }
+    }
+}
---
+++ Assets/Scripts/VRMode/IModeSwitcherService.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public interface IModeSwitcherService
+{
+    void ApplySelectedMode();
+    void EnableVRMode();
+    void EnableSimulatorMode();
+}
